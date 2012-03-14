41ece6613646369e96ec2b989cf72c24f47c8213
Germán <german_podesta@yahoo.com.ar>
2012-03-14 00:53:56 -0300

    The directory structure was reorganized. README file was added.

---
+++ tests/check_database.c
@@ -0,0 +1,56 @@
+#include "database.h"
+#include "cu/cu.h"
+#include <unistd.h>
+
+const char* DB_FILE="test.cdb";
+
+Package p={
+  .name = "Gcc",
+  .description = "GNU Compiler Collection (Conjunto de compiladores creados por el proyecto GNU). Es el compilador estandar para la mayoria de los sistemas operativos similares a Unix. Gcc soporta una gran cantidad de arquitecturas y puede compilar programas en C, C++, Fortran, Ada, Java, Objetive C, Pascal, entre otros.",
+  .section = "compilers",
+  .size = 102200121,
+  .web = "http://gcc.gnu.org/",
+  .maintainer = "core",
+  .version = 4000200,
+  .checksum = "1b0787539705920b5389766dd37114c1",
+};
+
+TEST(check_init_database){
+  unlink(DB_FILE); // Delete the file if exists
+  assertFalseM(init_database(DB_FILE),"Database initialization failed.");
+}
+
+TEST(check_add_package){
+  if ( open_database(DB_FILE) ){
+    assertTrueM(add_package(&p)==SQLITE_OK,"Failed adding a package.");
+    assertFalseM(add_package(&p)==SQLITE_OK,"Duplicated package should fail.");
+  } else {
+    assertTrueM(0,"Could not open the database.");
+  }
+  close_database();
+}
+
+TEST(check_add_relations){
+  Relation r1={
+    .name = "Binutils",
+    .comparator = ">=",
+    .version = 2210100,
+    .next = 0
+  };
+  if ( open_database(DB_FILE) ){
+    assertEqualsM(add_relations(p.name,'d',&r1),SQLITE_DONE,"Failed adding a relation.");
+    assertNotEqualsM(add_relations(p.name,'d',&r1),SQLITE_DONE,"Adding the same relation must be fail.");
+  } else {
+    assertTrueM(0,"Could not open the database.");
+  }
+  close_database();
+}
+
+TEST(check_find_id_package){
+  if ( open_database(DB_FILE) ){
+    
+  } else {
+    assertTrueM(0,"Could not open the database.");
+  }
+  close_database();
+}
---
+++ tests/check_database.h
@@ -0,0 +1,16 @@
+#include "cu/cu.h"
+
+// Declarations of tests
+TEST(check_init_database);
+TEST(check_add_package);
+TEST(check_add_relations);
+TEST(check_get_package);
+
+// Collect tests into test suite
+TEST_SUITE(database) {
+  TEST_ADD(check_init_database),
+  TEST_ADD(check_add_package),
+  TEST_ADD(check_add_relations),
+  TEST_ADD(check_get_package),
+  TEST_SUITE_CLOSURE
+};
