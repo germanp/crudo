bc29a9c33052ff00608b3f29ffa000489c3b50e3
Germán <german_podesta@yahoo.com.ar>
2012-07-26 00:53:51 -0300

    Ignored unity malloc (and family) overrides because bugs realloc.

--- test/unity/unity_fixture_malloc_overrides.h
+++ test/unity/unity_fixture_malloc_overrides.h
@@ -8,9 +8,9 @@
 #ifndef UNITY_FIXTURE_MALLOC_OVERRIDES_H_
 #define UNITY_FIXTURE_MALLOC_OVERRIDES_H_
 
-#define malloc  unity_malloc
-#define calloc  unity_calloc
-#define realloc unity_realloc
-#define free    unity_free
+/* #define malloc  unity_malloc */
+/* #define calloc  unity_calloc */
+/* #define realloc unity_realloc */
+/* #define free    unity_free */
 
 #endif /* UNITY_FIXTURE_MALLOC_OVERRIDES_H_ */
